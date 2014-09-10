c89f7e1192ec783874413de1ebd4ec55352446e8
fit087 <fitomillennium@hotmail.com>
2014-09-09 23:30:16 -0300

    Valores otimo e media
    São retornados os Valores otimo e media do PSO e eh trabalhado o
    criterio de parada sem atingir uma solução definitiva

--- PSO_func.m
+++ PSO_func.m
@@ -1,5 +1,5 @@
 % function [gbest,gbestval,fitcount]= PSO_func(fhd,Dimension,Particle_Number,Max_Gen,VRmin,VRmax,varargin)
-function [gbest,gbestval]= PSO_func(fhd,Dimension,Particle_Number,Max_Gen,VRmin,VRmax)
+function [gbest,gbestval,fitcount,beschi,media]= PSO_func(fhd,Dimension,Particle_Number,Max_Gen,VRmin,VRmax)
 %[gbest,gbestval,fitcount]= PSO_func('f8',3500,200000,30,30,-5.12,5.12)
 rand('state',sum(100*clock));
 me=Max_Gen;
@@ -23,16 +23,53 @@ pos=VRmin+(VRmax-VRmin).*rand(ps,D);
 
 e=feval(fhd,pos');
 
+% ******************************
+beschi(me)=0;
+media(me)=0;
+% ******************************
+
 fitcount=ps;
 vel=Vmin+2.*Vmax.*rand(ps,D);%initialize the velocity of the particles
 pbest=pos;
 pbestval=e; %initialize the pbest and the pbest's fitness value
 [gbestval,gbestid]=min(pbestval);
+
+% ******************************
+beschi(1)=gbestval;
+media(1)=mean(pbestval);
+% ******************************
+
 gbest=pbest(gbestid,:);%initialize the gbest and the gbest's fitness value
 gbestrep=repmat(gbest,ps,1);
 
-for i=2:me
+
+% ************************
+i=2;
+erro=1;
+% ************************
+
+% for i=2:me
+    contador=0;
+while(i<=me&(abs(erro)>10^-3|erro==0))
+%     while(i<=me&(abs(erro)>eps|erro==0))
 %         i
+
+%         rd=mod(i,35);%verificar. problema 50 e divisor exato de 100
+%         rd=mod(i,37);
+%         if rd==0
+% %         
+%         minimoDaFuncao=gbestval;
+%         
+%         end
+        
+%         rd=mod(i,37);
+%         if rd==0
+% %         
+%         minimoDaFuncao=gbestval;
+%         
+%         end
+
+%         minimoDaFuncao=gbestval;
         aa=cc(1).*rand(ps,D).*(pbest-pos)+cc(2).*rand(ps,D).*(gbestrep-pos);
         vel=iwt(i).*vel+aa;
         vel=(vel>Vmax).*Vmax+(vel<=Vmax).*vel;
@@ -49,7 +86,33 @@ for i=2:me
         pbestval=tmp.*pbestval+(1-tmp).*e;%update the pbest
         [gbestval,tmp]=min(pbestval);
         gbest=pbest(tmp,:);
+        %         ***************************************
+        beschi(i)=gbestval;            %guarda num vector os melhores
+        media(i)=mean(pbestval);
+%         ***************************************
         gbestrep=repmat(gbest,ps,1);%update the gbest
+        
+        %         *****************************************************************
+        i=i+1;
+%         erro=(gbestval-minimoDaFuncao);%/minimoDaFuncao;
+%         *****************************************************************
+
+
+  rd=mod(i,100);
+    if rd==0
+%         erro=abs(Fit(iBest)-valorAnterior);
+       
+%         erro=(gbestval-minimoDaFuncao);
+%         beschi(i-1)
+%         beschi(i-99)
+        contador=contador+1;
+        erro=(beschi(i-1)-beschi(i-99));
+        discrepancia(contador)=erro;
+        disp(['Fitness ', num2str(gbestval),'   Geração ',num2str(i),'  Erro   ',num2str(erro)]);
+%         erro
+    end
+
+
     end
 end
 
--- main_pso.m
+++ main_pso.m
@@ -10,16 +10,17 @@ MaxFES=D*10000*1;          % Número Máximo de Avaliações (número alto para deixa
 pop_size=40;               % Tamanho da população
 iter_max=MaxFES/pop_size;  % Numero máximo de gerações
 tolerancia=1e-3;           % Tolerância em unidade de metro cúbico
-runs=51;                    % Número de execuções
+runs=5;                    % Número de execuções
 fhd=str2func('Func_Manifolds'); % Chama a função objetivo -> ou fhd=@Func_Manifolds
 
 disp(['Número Máximo de Gerações: ',num2str(iter_max)]);
 
 for r=1:runs
         disp(['Rodada Número ',num2str(r)]);
-        [gbest,gbestval]= PSO_func(fhd,D,pop_size,iter_max,Xmin,Xmax);
+        [gbest,gbestval,Avaliacoes,otimo,media]= PSO_func(fhd,D,pop_size,iter_max,Xmin,Xmax);
         xbest(r,:)=gbest;        % Vetor X ótimo por rodada - Matriz (Num.Rodadas x Dimensao X) 
         fbest(r,:)=gbestval;     % Vetor Y ótimo por rodada - Matriz (Num.Rodadas x 1) 
         disp(['Mínimo da Rodada ',num2str(r),' = ',num2str(fbest(r,1))]);
+        NAval(r,:)=Avaliacoes;
 end
     f_mean=mean(fbest(r,:));   % Média dos Y de todas as rodadas
