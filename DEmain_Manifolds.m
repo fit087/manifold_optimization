5ebac96c808911ea1a6a9dfe0127cf1c556dd003
fit087 <fitomillennium@hotmail.com>
2014-09-09 23:03:46 -0300

    Formatação do DEmain
    Se deu um formato ao DEmain
    e se brincou com o MaxFES (nro maximo de avaliações) e com a tolerancia

--- DEmain_Manifolds.m
+++ DEmain_Manifolds.m
@@ -1,30 +1,27 @@
 %-------------------------------------------------------------%
-%                                                             %
 %         PROBLEMA DE LOCALIZAÇÃO DE MANIFOLDS (DE)           %
-%                                                             %
 %-------------------------------------------------------------%
-
 clear all
 format compact
 D=20;                      % Dimensões do problema = 20 (10 manifolds com X e Y)
 Xmin=-500;                 % Espaço de busca (x e y mínimo)
 Xmax=500;                  % Espaço de busca (x e y máximo)
-MaxFES=D*10000*1;          % Número Máximo de Avaliações
+MaxFES=D*10000*5;          % Número Máximo de Avaliações (número alto para deixar parar pela tolerância)
 pop_size=40;               % Tamanho da população
 iter_max=MaxFES/pop_size;  % Numero máximo de gerações
-tolerancia=1e-4;
-runs=1;                    % Número de execuções
+tolerancia=1e-3;           % Tolerância em unidade de metro cúbico
+runs=51;                    % Número de execuções
 fhd=str2func('Func_Manifolds'); % Chama a função objetivo -> ou fhd=@Func_Manifolds
 
-disp('Número Máximo de Gerações'); iter_max
+disp(['Número Máximo de Gerações: ',num2str(iter_max)]);
 
 for r=1:runs
-        r
-        [fotimo,xotimo]= DE_Func_Manifolds(fhd,D,pop_size,iter_max,Xmin,Xmax,tolerancia);
+        disp(['Rodada Número ',num2str(r)]);
+        [fotimo,xotimo,Avaliacoes]= DE_Func_Manifolds(fhd,D,pop_size,iter_max,Xmin,Xmax,tolerancia);
         xbest(r,:)=xotimo;     % Vetor X ótimo por rodada - Matriz (Num.Rodadas x Dimensao X) 
-        fbest(r,:)=fotimo;     % Vetor Y ótimo por funcao - Matriz (Num.Rodadas x 1) 
-        fotimo                 % Imprime Y (mínimo encontrado) para cada rodada
+        fbest(r,:)=fotimo;     % Vetor Y ótimo por rodada - Matriz (Num.Rodadas x 1) 
+        NAval(r,:)=Avaliacoes; % Vetor N avaliacoes por rodada - Matriz (Num.Rodadas x 1)
 end
     f_mean=mean(fbest(r,:));   % Média dos Y de todas as rodadas
-    fbest
+
 
